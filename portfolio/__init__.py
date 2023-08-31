c9a15be8d024419a7de25e28dd61caffe9d2cce1
Mubtunj1 <mubtunji01@gmail.com>
2023-08-31 11:42:19 +0100

    mail port

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -14,7 +14,7 @@ csrf = CSRFProtect(app)
 # Mail configuration
 mail = Mail()
 app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
-app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT"))
+app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT", 465))
 app.config["MAIL_USE_SSL"] = os.getenv("MAIL_USE_SSL") == "True"
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
